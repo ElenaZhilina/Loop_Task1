f51ba51bd76671599fda21b0608c74da9f195e1b
Elena <elenazhilina@yandex.ru>
2024-03-03 21:40:11 +0600

    Parametrized test

--- src/test/java/ru/netology/services/CalcVacationMonthTest.java
+++ src/test/java/ru/netology/services/CalcVacationMonthTest.java
@@ -1,34 +1,17 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CalcVacationMonthTest {
-    @Test
-    public void Example1() {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/Data.csv")
+    public void Examples(int expected, int income, int expenses, int threshold) {
         CalcVacationMonth service = new CalcVacationMonth();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
-
     }
-
-    @Test
-    public void Example2() {
-        CalcVacationMonth service = new CalcVacationMonth();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
 }
