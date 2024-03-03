6b33c9ea3c8593a9d7770a06480aa43c17ae631f
Elena <elenazhilina@yandex.ru>
2024-03-03 20:12:17 +0600

    Task1 completed

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,6 +1,12 @@
 package ru.netology;
 
 public class Main {
-    public static void main(String[] args) {}
-
+    public static void main(String[] args) {
+        CalcVacationMonth service = new CalcVacationMonth();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int actual = service.calculate(income, expenses, threshold);
     }
+
+}
---
+++ src/test/java/CalcVacationMonthTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.netology.CalcVacationMonth;
+
+public class CalcVacationMonthTest {
+    @Test
+    public void Example1() {
+        CalcVacationMonth service = new CalcVacationMonth();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void Example2() {
+        CalcVacationMonth service = new CalcVacationMonth();
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+}
